e6604a186df4d88e85d3709eefb3bb94009b615a
Nick Young <nick.young@auckland.ac.nz>
2016-06-16 14:14:34 +1200

    typo

--- private/parse_lowy.py
+++ private/parse_lowy.py
@@ -10,7 +10,7 @@ with open('../cc_names.json') as f:
 
 reverse_cc_names = {v: k for k,v in cc_names.items()}
 
-id = 121510000
+id = 121520000
 
 for entry in data['feed']['entry']:
   d = {'id': id}
@@ -27,4 +27,4 @@ for entry in data['feed']['entry']:
       if key == 'country':
         d['recipient_iso'] = reverse_cc_names[val]
       d[key] = val
-  print(u'INSERT INTO aid SET aiddate_id={id}, year={startdate}, donor="China", donor_iso="CN", donor_region="Far East Asia", implementing_agency="{actorschina}", commitment_amount_usd_constant={amount}, recipient="{country}", recipient_iso="{recipient_iso}", recipient_region="Oceania", title="{project}", long_description="{description}", aiddata_sector_name="{sector}", flow_name="{type}", source="LOWY";'.format(**d).encode('utf8'))
+  print(u'INSERT INTO aid SET aiddata_id={id}, year={startdate}, donor="China", donor_iso="CN", donor_region="Far East Asia", implementing_agency="{actorschina}", commitment_amount_usd_constant={amount}, recipient="{country}", recipient_iso="{recipient_iso}", recipient_region="Oceania", title="{project}", long_description="{description}", aiddata_sector_name="{sector}", flow_name="{type}", source="LOWY";'.format(**d).encode('utf8'))
